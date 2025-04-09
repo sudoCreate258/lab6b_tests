60fec94e948421aa3866374abd575461d24a6fb5
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2025-04-09 09:24:55 -0400

    Create test_func.py

---
+++ tests/test_func.py
@@ -0,0 +1,10 @@
+import pytest
+from functions import *
+
+def test_add():
+  assert add(1,2) ==  3
+  assert add([1],[2]) == [3]
+  assert add('5',1) == 6 
+  assert add(True,False) != 2
+  assert add([1],[2]) != [1,2]
+  assert add('5', '1') != '51'
