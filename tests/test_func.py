c98b31160099bd43510ff09ddc4eca76a4ced05e
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2025-04-10 08:33:38 -0400

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -3,8 +3,8 @@ from src.func import *
 
 def test_add():
   assert add(1,2) ==  3
-  assert add([1],[2]) == [3]
+  assert add([1],[2]) == [1,2]
   assert add('5',1) == 6 
   assert add(True,False) != 2
-  assert add([1],[2]) != [1,2]
+  assert add([1],[2]) != [3]
   assert add('5', '1') != '51'
